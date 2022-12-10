86a4bca7640a4bd0d555371ac6bd2bf92fecfbce
Ivan Gabriel <igabrielcorreopersonal2@gmail.com>
2022-12-09 20:48:28 -0800

    practica 2

--- Bang/ViewController.swift
+++ Bang/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate
+{
+    @IBOutlet var label: UILabel!
+    var audioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad()
+            {
+                super.viewDidLoad()
+                // Do any additional setup after loading the view.
+            }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    override func motionBegan( _ motion: UIEvent.EventSubtype, with event: UIEvent? )
+            {
+                guard let url = Bundle.main.url( forResource: "loud_bang", withExtension: "mp3" )
+                else
+                    {
+                        print( "Ocurrio un error" )
+                        return
+                    }
+                do
+                    {
+                            audioPlayer = try AVAudioPlayer(contentsOf: url)
+                            label.textColor = .red
+                            label.text = "Bang!"
+                            audioPlayer.play()
+                    }
+                catch{
+                    label.text = "Error al cargar archivo de audio"
+                }
+            }
 }
 
